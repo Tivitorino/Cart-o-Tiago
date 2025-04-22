285209b16f532fd13e696b8da19ddaea79cd9423
Tivitorino <thyagovitorinosouza@gmail.com>
2025-04-22 00:30:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function mostrarMensagem() {
+
+  alert("Obrigado por visitar meu portfólio, volte sempre!");
+  }
+  function alternarModo() {
+    document.body.classList.toggle("modo-claro");
+  }
